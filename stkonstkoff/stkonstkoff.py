8fec22a50c3a4baa08873016049741f4a7bef4c0
Bduncanson-17 <dunc0126@d.umn.edu>
2023-05-23 17:33:43 -0500

    Added some minor changes

--- stkonstkoff/stkonstkoff.py
+++ stkonstkoff/stkonstkoff.py
@@ -1,61 +1,52 @@
-import argparse
-
 import boto3
-from botocore.exceptions import ClientError
 
-from stkonstkoff.cloudformations.StackCreator import StackCreator
-from stkonstkoff.cloudformations.StackDeletor import StackDeletor
+class UserPermissions:
+    def __init__(self):
+        self.session = boto3.Session()
+        if self.check_boto3_permission():
+            self.has_cft_access = True
+        self.has_cft_access = False
 
-
-class StkOnStkOffUser:
-    def __init__(self, session=None):
-        self.session = session if session else boto3.Session()
-        self.authorized = None
-        self.cft_iam_access = []
-        self.stack_operation = None
-
-    def check_aws_credentials(self):
+    def check_boto3_permission(service):
         try:
-            self.session.client('sts').get_caller_identity()
-            self.authorized = True
+            # Attempt to create a client or resource object
+            client = boto3.client(service)
+            return True
         except Exception as e:
-            print("Failed to check AWS credentials:", str(e))
-            self.authorized = False
+            # If an exception occurs, it means the session does not have permission to use Boto3
+            return False
 
-    def set_aws_credentials(self):
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--aws_access_key_id", help="Your AWS Access Key ID")
-        parser.add_argument("--aws_secret_access_key", help="Your AWS Secret Access Key")
-        parser.add_argument("--aws_session_token", help="Your AWS Session Token")
-        parser.add_argument("--region", help="AWS Region")
-        args = parser.parse_args()
-
-        self.session = boto3.Session(
-            aws_access_key_id=args.aws_access_key_id,
-            aws_secret_access_key=args.aws_secret_access_key,
-            aws_session_token=args.aws_session_token,
-            region_name=args.region
+    def set_aws_credentials(self, aws_access_key_id, aws_secret_access_key, aws_session_token, region):
+        new_session = boto3.Session(
+            aws_access_key_id=aws_access_key_id,
+            aws_secret_access_key=aws_secret_access_key,
+            aws_session_token=aws_session_token,
+            region_name=region
         )
+        self.session = new_session
+        self.check_boto3_permission("cloudformations")
 
-    def set_cloudformations_iam(self):
-        iam = self.session.client('iam')
-        try:
-            # assuming the user's name is available in the caller identity
-            username = self.session.client('sts').get_caller_identity().get('Arn').split('/')[-1]
-            policies = iam.list_policies_for_user(UserName=username)
-            self.cloudformations_iam = [policy.get('PolicyName') for policy in policies.get('Policies')]
-        except ClientError as e:
-            print("Failed to retrieve IAM policies:", str(e))
 
-    def set_stack_operation(self, operation):
-        self.set_cloudformations_iam()
-        if self.cft_iam_access is None or len(self.cft_iam_access) == 0:
-            return None
-        if operation.lower() == "create":
-            self.stack_operation = StackCreator(self.session)
-        elif operation.lower() == "delete":
-            self.stack_operation = StackDeletor(self.session)
-        else:
-            print(f"Invalid operation {operation}")
+class CloudFormationStacks:
+    def __init__(self, authorized_user=UserPermissions()):
+        self.stacks = None
+        if authorized_user.has_cft_access:
+            self.stacks = self.current_stacksget_user
+        self.Creator = None
+        self.Deleter = None
 
-awsuser = aw
+
+
+    def get_user_stacks(self):
+        response = self.client.list_stacks(StackStatusFilter=['CREATE_COMPLETE', 'UPDATE_COMPLETE'])
+        stacks = response.get('StackSummaries', [])
+        return stacks
+
+    def list_stack_attribute(self):
+        if self.stacks is None or len(self.stacks) == 0:
+            print("Current no stacks are active")
+        for stack in self.stacks:
+            print(stack['StackName'])
+
+
+# Print information about the user stacks
